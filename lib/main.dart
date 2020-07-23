b31a95e39f61685365d93b9296d106dff0145b7e
Dustin <p4569zz@gmail.com>
2020-07-23 16:37:27 +0900

    prcatice layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('Hello Flutter'),
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Row(
+            //verticalDirection: VerticalDirection.down,
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+//            margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+//            padding: EdgeInsets.all(20.0),
+                color: Colors.white,
+                child: Text('Container1'),
+              ),
+              SizedBox(width: 20.0),
+              Container(
+                color: Colors.lightBlue,
+                child: Text('Container2'),
+              ),
+              Container(
+                color: Colors.red,
+                child: Text('Container3'),
+              ),
+              Container(
+                color: Colors.red,
+                child: Text('Container3'),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
+
+//Hot Reload
